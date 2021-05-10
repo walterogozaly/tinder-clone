d16208dc2e5c7c3e3ec57542a2fe4b9114372f3a
walter97 <walter97ogozaly@gmail.com>
2021-05-10 12:39:55 -0400

    First Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Let's build the MERN Tinder!</h1>
+
+      {/* Header */}
+      {/* TinderCards */}
+      {/* Swipe buttons */}
+      
+    </div>
+  );
+}
+
+export default App;
