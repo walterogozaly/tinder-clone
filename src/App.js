89dfebaba7d44a880770cbe56f3e075803f64a39
walter97 <walter97ogozaly@gmail.com>
2021-05-12 13:07:45 -0400

    Finished cards

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from "./Header";
 import TinderCards from "./TinderCards";
+import SwipeButtons from "./SwipeButtons";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
 
       <Header />
       <TinderCards />
-      {/* Swipe buttons */}
+      <SwipeButtons />
 
     </div>
   );
--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,24 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "./TinderCards.css";
 import TinderCard from "react-tinder-card";
+import { SwipeableDrawer } from '@material-ui/core';
+import axios from "./axios";
 
 function TinderCards() {
-  const [people, setPeople] = useState([
-    {
-      name: 'Peter Thiel',
-      url: "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5a836fff31358e4955ad6549%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D234%26cropX2%3D790%26cropY1%3D7%26cropY2%3D563"
-    },
-    {
-      name: 'Jane Fraser',
-      url: "https://media4.s-nbcnews.com/j/newscms/2020_37/3411147/200910-jane-fraser-ew-1102a_93d96b62877506c4767afe48ed6fd5ef.fit-760w.jpg"
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const req = await axios.get("/tinder/cards");
+
+      setPeople(req.data)
     }
-  ]);
+
+    fetchData();
+  }, []);
+
+  console.log(people);
+
+  const swiped = (direction, nameToDelete) => {
+    console.log("removing: " + nameToDelete);
+    //setLastDirection(direction);
+  }
+
+  const outOfFrame = (name) => {
+    console.log(name + " left the screen!");
+  }
+
 
   return (
     <div className='tinderCards'>
       <div className="tinderCards__cardContainer">
         {people.map(person => (
-          <TinderCard>{person.name}</TinderCard>
+          <TinderCard
+            className="swipe"
+            key={person.name}
+            preventSwipe={["up", "down"]}
+            //onSwipe={(dir) => SwipeableDrawer(dir, person.name)}
+            onCardLeftScreen={() => outOfFrame(person.name)}
+          >
+            <div
+              style={{ backgroundImage: `url(${person.imgUrl})` }}
+              className="card"
+            >
+              <h3>{person.name}</h3>
+            </div>
+          </TinderCard>
         ))}
       </div>
     </div>
